1878ab3f6471c67c49d96964634a4c525f727287
gene <pocciaeugenio@gmail.com>
2019-05-13 12:16:18 -0400

    routes for subforums

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -31,6 +31,30 @@ class UnconnectedApp extends Component {
     );
   };
 
+  renderBasketball = () => {
+    return <div>Basketball subforum</div>;
+  };
+
+  renderHockey = () => {
+    return <div>Hockey subforum</div>;
+  };
+
+  renderRockClimbing = () => {
+    return <div>Rock Climbing subforum</div>;
+  };
+
+  renderSoccer = () => {
+    return <div>Soccer subforum</div>;
+  };
+
+  renderTennis = () => {
+    return <div>Tennis subforum</div>;
+  };
+
+  renderMisc = () => {
+    return <div>Miscallenous sports subforum</div>;
+  };
+
   render = () => {
     return (
       <BrowserRouter>
@@ -45,6 +69,20 @@ class UnconnectedApp extends Component {
             path="/UserDetails"
             render={this.renderUserDetails}
           />
+          <Route
+            exact={true}
+            path="/Basketball"
+            render={this.renderBasketball}
+          />
+          <Route exact={true} path="/Hockey" render={this.renderHockey} />
+          <Route
+            exact={true}
+            path="/RockClimbing"
+            render={this.renderRockClimbing}
+          />
+          <Route exact={true} path="/Soccer" render={this.renderSoccer} />
+          <Route exact={true} path="/Tennis" render={this.renderTennis} />
+          <Route exact={true} path="/Misc" render={this.renderMisc} />
         </div>
       </BrowserRouter>
     );
