3b937bccedb1101784f88c4504800ba5581323a0
Paul Jeambrun <paulj@MacBook-Pro-de-Paul.local>
2019-05-13 12:30:25 -0400

    topbar

---
+++ frontend/src/Signup.jsx
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+class UnconnectedSignup extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: ""
+    };
+  }
+
+  handleUsernameChange = event => {
+    this.setState({ username: event.target.value });
+  };
+
+  handlePasswordChange = event => {
+    this.setState({ password: event.target.value });
+  };
+  handleSubmit = evt => {
+    evt.preventDefault();
+    console.log("hit signup");
+    let data = new FormData();
+    data.append("username", this.state.username);
+    data.append("password", this.state.password);
+    fetch("http://localhost:4000/signup", { method: "POST", body: data });
+    alert("signup successful!");
+    this.props.dispatch({ type: "login-success" });
+  };
+
+  render = () => {
+    return (
+      <div className="signup">
+        <h3>Sign-up</h3>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            className="pass"
+            type="text"
+            onChange={this.handleUsernameChange}
+            placeholder="Username"
+          />
+          <input
+            className="pass"
+            type="text"
+            onChange={this.handlePasswordChange}
+            placeholder="Password"
+          />
+          <input className="submit" type="submit" />
+        </form>
+      </div>
+    );
+  };
+}
+let Signup = connect()(UnconnectedSignup);
+export default Signup;
--- frontend/src/TopBar.jsx
+++ frontend/src/TopBar.jsx
@@ -1,20 +1,36 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { BrowserRouter, Route, Link } from "react-router-dom";
 
 class UnconnectedTopBar extends Component {
   render = () => {
-    return (
-      <div>
-        <div className="flex">
-          <div>
-            <Link to="/">MTLSport</Link>
-          </div>{" "}
-          <div>
-            <Link to="/myAccount">Login/Signup</Link>
-          </div>{" "}
+    if (!this.props.loggedIn) {
+      return (
+        <div>
+          <div className="flex">
+            <div>
+              <Link to="/">MTLSport</Link>
+            </div>{" "}
+            <div>
+              <Link to="/form">Login/Signup</Link>
+            </div>{" "}
+          </div>
         </div>
-      </div>
-    );
+      );
+    } else {
+      return (
+        <div>
+          <div className="flex">
+            <div>
+              <Link to="/">MTLSport</Link>
+            </div>{" "}
+            <div>
+              <Link to="/myAccount">My account</Link>
+            </div>{" "}
+          </div>
+        </div>
+      );
+    }
   };
 }
 
