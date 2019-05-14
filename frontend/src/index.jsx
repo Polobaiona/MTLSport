208c509d5b99af6d59609c6ad6acc5810024fab5
gene <pocciaeugenio@gmail.com>
2019-05-14 12:30:11 -0400

    removed store cause im dumb, bringbrandonback2019

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -3,7 +3,7 @@ import "./main.css";
 import React from "react";
 import { Provider } from "react-redux";
 import App from "./App.jsx";
-import store from "./store.jsx";
+import store from "./store.js";
 
 ReactDOM.render(
   <Provider store={store}>
